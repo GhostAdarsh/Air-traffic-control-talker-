e07e263eee989c6db721d9142a44d95eaaf14ded
GhostAdarsh <19-adarsh.chakka@whsg.info>
2025-10-07 09:39:55 +0100

    Update newlibtest.py

--- newlibtest.py
+++ newlibtest.py
@@ -4,12 +4,16 @@ import pygame
 
 pygame.init()
 img = cv2.imread('LHRNEA.png')
+screen = pygame.display.set_mode(1280, 800)
 res = cv2.resize(img, dsize=(54, 140), interpolation=cv2.INTER_CUBIC)
 running = True
 while running: 
-    for events in pygame.event.get():
+    for event in pygame.event.get():
+        screen.blit(img, (0,0))
+        
 
         if event.type == pygame.QUIT:
             running = False
+
 
 
