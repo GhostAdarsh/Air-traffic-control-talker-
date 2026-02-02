8c2088c6cbe44e38d43d7b40a51870e7d62dcb65
Adarsh7567 <19-adarsh.chakka@whsg.info>
2026-02-02 11:09:46 +0000

    Update maya.py

--- maya.py
+++ maya.py
@@ -2,6 +2,8 @@ import speech_recognition as sr
 import pyttsx3
 import pygame
 import pyaudio 
+import google 
+import smtplib
 pygame.init() 
 r = sr.Recognizer()
 
@@ -19,7 +21,15 @@ class Planes:
 
     def record_Audio(): 
         print("x")
+        with sr.Microphone() as mic: 
 
+            r.adjust_for_ambient_noise(mic, duration = 0.2)
+            audio = r.listen(mic)
+
+            text = r.recognize_google_cloud(audio)
+            text = text.lower() 
+
+            print(f"Recognised {text}")
 
 running = True 
 while running: 
@@ -27,14 +37,7 @@ while running:
     screen.fill((0,0,0))
     screen.blit(background, (0, 0))
     #speechrecog
-    with sr.Microphone() as mic:
-        r.adjust_for_ambient_noise(mic, duration=0.2)
-        audio = r.listen(mic)
-
-        text = r.recognize_amazon(audio)
-        text = text.lower()
-
-        print(f"Recognized {text}")
+    
         
     for event in pygame.event.get():
         # if tab key pressed, quits game
@@ -43,3 +46,5 @@ while running:
             pygame.quit()
         if event.type == pygame.QUIT: 
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            Planes.record_Audio()
