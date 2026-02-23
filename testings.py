1ff6d529d2b76585d479b3fe3932555a306f62a9
GhostAdarsh <19-adarsh.chakka@whsg.info>
2026-02-23 11:10:29 +0000

    Update testings.py

--- testings.py
+++ testings.py
@@ -1,27 +1,198 @@
-# this is to stage tests from all github uploads: 
+# stage testings: 
 
-print("x")
+# import modules and objects here: 
+import pygame 
+from pygame import * 
+import matplotlib 
+from matplotlib import pyplot as plt 
+from matplotlib import image 
 import pathfinding 
-from pathfinding import * 
 from pathfinding.core.grid import Grid 
 from pathfinding.finder.a_star import AStarFinder
+from pathfinding.core.diagonal_movement import DiagonalMovement 
 
-matrix = [
-    1, 1, 1, 1, 1,
-    1, 0, 1, 1, 1,
-    1, 1, 1, 1, 1,
-]
-grid = Grid(matrix= matrix) 
 
-# start and end nodes 
-start = grid.node(0,0)
-end = grid.node(5,2)
+#from objectplanes import Planes
 
-#creaste a finder with a movement style 
-finder = AStarFinder
+
+
+# class pathfinder 
+
+class Pathfinder:
+            
+
+        def __init__(self, matrix): 
+              self.matrix = matrix 
+              self.grid = Grid(matrix = matrix)
+              self.select_surf = pygame.image.load('crosshairX.png').convert_alpha()
+
+              # pathfinding 
+              self.path = [] 
+        def draw_active_cell(self):
+                # the mouse part isnt needed in the main.py code 
+                mouse_pos = pygame.mouse.get_pos()
+                print(mouse_pos)
+                row = mouse_pos[1] // 32 
+                col = mouse_pos[0] // 32 
+                rect = pygame.Rect((col * 32, row * 32), (32,32))
+                screen.blit(self.select_surf, rect)
+        def create_path(self):
+                # start pt 
+                start_x, start_y =  [1,1]
+                start = self.grid.node(start_x, start_y)
+
+                #end pt 
+                mouse_pos = pygame.mouse.get_pos()
+                end_x, end_y = mouse_pos[0] // 32, mouse_pos[1] // 32
+                end = self.grid.node(end_x, end_y)
+         
+                # path 
+                finder = AStarFinder(diagonal_movement =  DiagonalMovement.always)
+                self.path = finder.find_path(start, end, self.grid)
+                print(self.path)
+        def update(self):
+              self.draw_active_cell()
+              self.create_path() 
+
+print("X")
+
+# TASK A - load an image and setr it as background - DONE
+
+#create screen
+screen = pygame.display.set_mode((1280,800))
+
+#add background
+background = pygame.image.load("LondonHeathrowNEA.jpg")
+
+#scale background 
+pygame.transform.scale(background, (50, 50))
+# set icon 
+win_icon = pygame.image.load("flightradar24.jfif")
+pygame.display.set_icon(win_icon)
+
+#set window title 
+pygame.display.set_caption("Air Traffic Talker")
+
+# coordinate points - time consuming do this @ home (did this only for T5 PLANES)
+points = [(360, 390), (360, 450), (490, 410), (407, 400), (429,400), (469, 450)]
+# checkpoint coordinates - this is the junctions at the taxiway - takeoff and landing only  
+checkpts = [(384, 358), (320,340), ]
+landpts = [(300, 300), (400, 400)]
+
+
+#create clock 
+clock = pygame.time.Clock()     
+
+
+
+# matrix to map out everything 
+# problem - if i was to take the orig image there would be SO MANY 0s  on the preimeter due to the abstraction of the outside | 40 point mark (stright down)
+# wld i have to code the entire image matrix 
+matrix = [  
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0],
+      ]
+
+grid = Grid(matrix= matrix)
+
+# start and end 
+
+start = grid.node( 0,0 )
+end = grid.node( 5, 6 )
+
+# movement style 
+finder = AStarFinder()
 
 # use finder to find path 
-path,runs = finder.find_path(start,end, grid) 
+path,runs = finder.find_path(start, end, grid)
 
-# print result 
-print(path) 
+
+
+
+
+
+
+# while loop to keep code running 
+running = True
+while running: 
+    #colour scheme 
+    screen.fill((0,0,0))
+    screen.blit(background, (0, 0))
+    for event in pygame.event.get():
+        # if tab key pressed, quits game
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_TAB]:
+            pygame.quit()
+        if event.type == pygame.QUIT: 
+            running = False
+
+    # draws the coordinate pts 
+    for point in points: 
+            pygame.draw.circle(screen, "red", point, 5)
+    for checkpt in checkpts: 
+            pygame.draw.circle(screen, "green", checkpt, 3)
+    for landpt in landpts:
+            pygame.draw.circle(screen, "blue", landpt, 3)
+        
+    clock.tick(100)   
+        
+        
+
+
+
+
+
+
+    pygame.display.update()
+
+
+# TASK  B - create and plot coordinate on the 15 places on the background - set these as finishing points - DONE (lines  28 and 54 above)
+
+# TASK C - create object (planes) - set image to them and make them follow a path - diffictult - use OOP 
+# split task c to 2 pts 
+# watch pathfinding algorithm found on yt - work on that eg 
+a = Planes("BA277")
+b = Planes("BA776")
+c = Planes("BA928")
+d = Planes("")     
+e = Planes("")
+
+departures = [a, b, c, d, e]
+
+for departure in departures():
+      print("X")
+
+
+
+pygame.quit() 
