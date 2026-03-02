a426eaf2f2e8feb35dab53f3f68dcc0ed7acdd48
Adarsh7567 <19-adarsh.chakka@whsg.info>
2026-03-02 16:37:38 +0000

    works

--- voiceCommand.py
+++ voiceCommand.py
@@ -7,7 +7,7 @@ import json
 from vosk import Model, KaldiRecognizer
 
 #load model 
-print("hell0")
+#print("hell0")
 
 q = queue.Queue() 
 
@@ -27,12 +27,16 @@ class VoiceControl:
         #recogniser 
         self.recogniser = KaldiRecognizer(self.model, 16000, json.dumps(self.vocab))
 
+
+        # test data: 
+        test_input = "speedbird one two three crealed takeoffrunway two seven"
+
         # creatubg a queue
         self.q = queue.Queue()
 
+        parsed = self.parse_command(test_input)
 
-        #vocab
-        
+        print(parsed)
 
         def callback(indata, frames, time, status):
             self.q.put(bytes(indata))
@@ -47,9 +51,6 @@ class VoiceControl:
 
         self.stream.start()
 
-
-
-
     def recognise_command(self):
         
         # setting the audio specifics  
@@ -60,10 +61,14 @@ class VoiceControl:
         while True: 
             data = self.q.get() 
 
+            
+
             if self.recogniser.AcceptWaveform(data): 
                     result = json.loads(self.recogniser.Result())
                     text =  result.get("text", "")
                     text = text.strip().lower() 
+                    return self.parse_command(text)
+            
 
                     valid_words = [] 
 
@@ -71,25 +76,21 @@ class VoiceControl:
                         if command in text: 
                             valid_words.append(command)
 
-                    if valid_words: 
+                    '''if valid_words: 
                         print("VALID commmand:", valid_words)
                         return valid_words
                     else: 
                         print("invalid", text)
-                        return None
-
-
-                    # checks if word is in self.vocab 
-                    '''if text in self.vocab: 
-                        print("Valid command", text)
-                        return text
-                    else: 
-                        print("invalid - ignoring", text)
                         return None'''
+
+
                     
                     if text != "":
                         print("recognised", text)
                         return text
+            
+     
+    
 
                 
 
@@ -97,12 +98,15 @@ class VoiceControl:
 
 
 
-
+## main processing 
     def process_voice(self):
         text = self.recognise_command() 
+        print(text)
+
         if not text: 
             self.computer_voice("Repeat")
             return
+        
         command = self.parse_command(text)
         if not command: 
             self.computer_voice("Invalid Command")
@@ -112,8 +116,30 @@ class VoiceControl:
         self.execute_command(command)
 
     
-    def parse_command(self, text): 
-        pass
+    def parse_command(self, text):
+        #standardises the text 
+        text = text.lower().strip() 
+        words = text.split() 
+
+        command  = {
+            "callsign": None,
+            "action": None,
+            "runway": None, 
+            "holding_point": None 
+        }
+        if "takeoff" in words: 
+            command["action"] = "takeoff"
+            print("aciton - takeoff")
+
+        elif "taxi" in words: 
+            command["action"] = "taxi"
+            print("action - taxi")
+
+        elif "hold" in words: 
+            command["action"] = "hold"
+            print("action - hold")
+
+
     def execute_command(self, command):
         pass
     def computer_voice(self, message): 
