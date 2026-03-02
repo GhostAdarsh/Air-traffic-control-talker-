e112c29d97d8f3690e676a3ff0ecdd69488259de
Adarsh7567 <19-adarsh.chakka@whsg.info>
2026-03-02 16:03:09 +0000

    ??

--- voiceCommand.py
+++ voiceCommand.py
@@ -7,8 +7,7 @@ import json
 from vosk import Model, KaldiRecognizer
 
 #load model 
-model = Model("vosk-model-smallen-us-0.15")
-recognizer = KaldiRecognizer(model, 16000)
+print("hell0")
 
 q = queue.Queue() 
 
@@ -20,7 +19,85 @@ class VoiceControl:
     def __init__(self):
         self.activeAirctaft = aircraft_list
         self.valid_holding_points = holding_points
-    
+
+        # intialising vosk 
+        self.model = Model("vosk-model-small-en-us-0.15")
+        # list of words 
+        self.vocab = ["takeoff", "runway", "hold", "taxi", "holding point"]
+        #recogniser 
+        self.recogniser = KaldiRecognizer(self.model, 16000, json.dumps(self.vocab))
+
+        # creatubg a queue
+        self.q = queue.Queue()
+
+
+        #vocab
+        
+
+        def callback(indata, frames, time, status):
+            self.q.put(bytes(indata))
+
+        self.stream = sd.RawInputStream(
+            samplerate= 16000, 
+            blocksize= 8000, 
+            dtype= 'int16', 
+            channels= 1,
+            callback=callback
+        )
+
+        self.stream.start()
+
+
+
+
+    def recognise_command(self):
+        
+        # setting the audio specifics  
+        
+        print("listening")
+
+            # starts recognising the voice from mic
+        while True: 
+            data = self.q.get() 
+
+            if self.recogniser.AcceptWaveform(data): 
+                    result = json.loads(self.recogniser.Result())
+                    text =  result.get("text", "")
+                    text = text.strip().lower() 
+
+                    valid_words = [] 
+
+                    for command in self.vocab: 
+                        if command in text: 
+                            valid_words.append(command)
+
+                    if valid_words: 
+                        print("VALID commmand:", valid_words)
+                        return valid_words
+                    else: 
+                        print("invalid", text)
+                        return None
+
+
+                    # checks if word is in self.vocab 
+                    '''if text in self.vocab: 
+                        print("Valid command", text)
+                        return text
+                    else: 
+                        print("invalid - ignoring", text)
+                        return None'''
+                    
+                    if text != "":
+                        print("recognised", text)
+                        return text
+
+                
+
+
+
+
+
+
     def process_voice(self):
         text = self.recognise_command() 
         if not text: 
@@ -34,12 +111,7 @@ class VoiceControl:
         
         self.execute_command(command)
 
-    def recognise_command(self):
-        print("Listening...")
-        while True: 
-            data = self.q.get() 
-            if self.recognizer.AcceptWaveform(data): 
-                result = json.loads(self.recognizer.Result()) 
+    
     def parse_command(self, text): 
         pass
     def execute_command(self, command):
@@ -53,3 +125,7 @@ class VoiceControl:
 print("hello world")
 print("this is to be done by monday")
 
+#VoiceControl.recognise_command()
+
+voice = VoiceControl() 
+print(voice.recognise_command())
